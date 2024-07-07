f0b1fd947c5fc5be025e9357421dbc9f6facd200
Otaviano Manoel <otavianomanoelvasconcelos@gmail.com>
2024-07-07 10:54:29 -0300

    ajustes no template

---
+++ src/Pages/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import styled from './app.module.scss';
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
